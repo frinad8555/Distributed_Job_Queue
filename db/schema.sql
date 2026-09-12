f7a35160144f49e505ec1a603ba7d0313913d550
frinad.8555 <frinadvk2006@gmail.com>
2026-09-13 02:52:04 +0530

    Proper definition of the table as of now

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,14 @@ CREATE TABLE jobs (
     type VARCHAR(100) NOT NULL,
     payload JSONB NOT NULL,
     status VARCHAR(20) NOT NULL DEFAULT 'QUEUED',
+    priority INT NOT NULL DEFAULT 0,
+
+    retry_count INT NOT NULL DEFAULT 0,
+    max_retries INT NOT NULL DEFAULT 3,
+    next_retry_at TIMESTAMP,
+
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     started_at TIMESTAMP,
-    completed_at TIMESTAMP
+    completed_at TIMESTAMP,
+    last_heartbeat TIMESTAMP
 );
